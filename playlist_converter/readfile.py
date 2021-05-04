de0decd19dfe5e603e36a6180a486745be5a020f
Priya Aggarwal <priya.aggarwal0125@gmail.com>
2021-05-03 22:06:52 -0400

    Create class to get playlist data from files

--- playlist_converter/readfile.py
+++ playlist_converter/readfile.py
@@ -2,6 +2,42 @@ import os
 from typing import List, Tuple, Union
 
 
+class PlaylistFile:
+    """File representation of a playlist."""
+
+    def __init__(self, path: str):
+        self.path = path
+        self.lines = self.clean_lines()
+
+    def clean_lines(self) -> List[str]:
+        with open(self.path, "r") as f:
+            lines = f.readlines()
+        return [ln.strip() for ln in lines]
+
+    def line_starts_with(self, prefix: str) -> Union[str, None]:
+        for ln in self.lines:
+            if ln.lower().startswith(prefix):
+                return ln
+        return None
+
+    def playlist_name(self) -> Union[str, None]:
+        # Assumption: Line with playlist name begins with "name:"
+        line = self.line_starts_with("name:")
+        if not line:
+            return None
+        colon_index = line.index(":")
+        return line[colon_index + 1:].strip()
+
+    def playlist_items(self, delimiter: str) -> List[Tuple[str, str]]:
+        items = []
+        # Assumption: Track name and artists separated by delimiter
+        for ln in self.lines:
+            content = ln.split(delimiter)
+            if len(content) >= 2:
+                items.append((content[0].strip(), content[1].strip()))
+        return items
+
+
 def directory_files(path: str) -> List[str]:
     filenames = []
     contents = os.listdir(path)
@@ -23,4 +59,12 @@ def directory_textfiles(path: str) -> List[str]:
         message = '"{}" contains no textfiles'.format(path)
         raise FileNotFoundError(message)
     return textfiles
+
+def get_playlists(directory_path: str) -> List[PlaylistFile]:
+    playlists = []
+    textfiles = directory_textfiles(directory_path)
+    for tf in textfiles:
+        tf_path = os.path.join(directory_path, tf)
+        playlists.append(PlaylistFile(tf_path))
+    return playlists
 
